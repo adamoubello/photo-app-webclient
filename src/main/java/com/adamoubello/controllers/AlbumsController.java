b7b4b33addc4052518f3fad7ba1e07564efd9799
Bello the son of Sun <adamou.bello@gmail.com>
2024-08-16 09:47:09 +0200

    photo-app-webclient: working photo-app-webclient sample

---
+++ src/main/java/com/adamoubello/controllers/AlbumsController.java
@@ -0,0 +1,42 @@
+package com.adamoubello.controllers;
+
+import com.adamoubello.response.AlbumRest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.List;
+
+@Controller
+public class AlbumsController {
+	
+	@Autowired
+	OAuth2AuthorizedClientService oauth2ClientService;
+
+	@Autowired
+	WebClient webClient;
+
+	@GetMapping("/albums")
+	public String getAlbums(Model model, 
+			@AuthenticationPrincipal OidcUser principal) {
+
+		String url = "http://localhost:8082/albums";
+
+		List<AlbumRest> albums = webClient.get()
+				.uri(url)
+				.retrieve()
+				.bodyToMono(new ParameterizedTypeReference<List<AlbumRest>>(){})
+				.block();
+	
+        model.addAttribute("albums", albums);
+		
+		return "albums";
+	}
+	
+}
